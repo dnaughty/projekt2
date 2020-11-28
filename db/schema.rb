c0aa4a3e13173bc478b5ec146dd61b74396fdad8
Denis Naughton <mjolinor75@hotmail.com>
2020-11-27 20:20:18 -0500

    added some controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_214145) do
+ActiveRecord::Schema.define(version: 2020_11_27_215422) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_11_27_214145) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
